8f5a5db59b593ac00756041988909bc5291168ef
Muhammad Rizky Firdaus <62287144+ubudab109@users.noreply.github.com>
2024-01-02 00:53:50 +0700

    Update Show.jsx

--- resources/js/Pages/Admin/BatchPayment/Show.jsx
+++ resources/js/Pages/Admin/BatchPayment/Show.jsx
@@ -232,13 +232,13 @@ export default function Show(props) {
                         </a>
                     ) : ''} */}
 
-                            <button onClick={() => submitProcess()}>
-                                <button class="inline-flex items-center px-4 py-2 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 undefined ">
-                                    Submit
+                            {props.data.user_role.role_id == props.data.batch_payment.role_id && props.data.batch_payment.status == 'on progress' ? (
+                                <button onClick={() => submitProcess()}>
+                                    <button class="inline-flex items-center px-4 py-2 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 undefined ">
+                                        Submit
+                                    </button>
                                 </button>
-                            </button>
-                            {/* {props.data.user_role.role_id == props.data.batch_payment.role_id && props.data.batch_payment.status == 'on progress' ? (
-                    ) : ''} */}
+                            ) : ''}
 
                         </div>
                     </dic>
